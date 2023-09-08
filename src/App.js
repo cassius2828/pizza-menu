950aea5538add2e5f20ad8fce98ea8a1c0c58c33
Cassius Reynolds <cassius2828@gmail.com>
2023-09-07 20:55:03 -0700

    added time

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import "./App.css";
 import tachyons from "tachyons";
 import { Cards } from "./components/Cards";
 import { pizzaData } from "./data";
-
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [storeOpen, setStoreOpen] = useState(true);
+  const [hour, setHour] = useState(new Date().getHours());
+  const [minute, setMinute] = useState(new Date().getMinutes());
+
+ 
+  useEffect(() => {
+    console.log(hour);
+    console.log(minute);
+  }, []);
   return (
     <div className="App">
       <h1>- fast react pizza co. -</h1>
@@ -18,18 +26,27 @@ function App() {
         <p className="bold"></p>
         <div className="card-container">
           {pizzaData.map((i) => {
-           return <Cards
-              image={i.photoName}
-              name={i.name}
-              ingredients={i.ingredients}
-              price={i.price}
-              isSoldOut={i.soldOut}
-            />;
+            return (
+              <Cards
+                image={i.photoName}
+                name={i.name}
+                ingredients={i.ingredients}
+                price={i.price}
+                isSoldOut={i.soldOut}
+              />
+            );
           })}
         </div>
       </div>
       <footer className="mt5 mb5">
-        <p className="tc">We are open until 10:00pm. Come visit us now or order online</p>
+        {(hour > 22 && minute > 59) || hour < 9 ? (
+          <p className="tc">We will reopen at 9:00 AM. Hope to see you soon!</p>
+        ) : (
+          <p className="tc">
+            We are open until 10:00 PM! Come visit us now or order online
+          </p>
+        )}
+      
 
         <button className="mt3">Order</button>
       </footer>
